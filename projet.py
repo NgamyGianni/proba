f62f2a4859bf03a4bfedec4c53ed89b68089e420
Gianni-Alessandro ngamy <gianningamy@MacBook-Pro-de-Gianni-Alessandro.local>
2020-11-09 14:53:17 +0100

    MAPNaive

--- projet.py
+++ projet.py
@@ -46,18 +46,19 @@ class APrioriClassifier(utils.AbstractClassifier):
 
 def P2D_l(df, attr):
     l = pd.crosstab(df.target, [df[attr]])
+    tmp = {e for e in df[attr]}
     res = {0: {e: {} for e in df[attr]}, 1: {e: {} for e in df[attr]}}
     sm0 = 0
     sm1 = 0
 
-    for i in range(int(l.size / 2)):
+    for i in tmp:
         sm0 += l[i][0]
         sm1 += l[i][1]
 
     d0 = res[0]
     d1 = res[1]
 
-    for i in range(int(l.size / 2)):
+    for i in tmp:
         d0[i] = l[i][0] / sm0
         d1[i] = l[i][1] / sm1
 
@@ -66,9 +67,10 @@ def P2D_l(df, attr):
 
 def P2D_p(df, attr):
     l = pd.crosstab(df.target, [df[attr]])
+    tmp = {e for e in df[attr]}
     res = {e: {} for e in df[attr]}
 
-    for i in range(len(res)):
+    for i in tmp:
         res[i] = {1: l[i][1] / (l[i][0] + l[i][1]), 0: l[i][0] / (l[i][0] + l[i][1])}
     return res
 
@@ -105,7 +107,7 @@ class ML2DClassifier(APrioriClassifier):
 
         return {"VP": vp, "VN": vn, "FP": fp, "FN": fn, "Précision": vp / (vp + fp), "Rappel": vp / (vp + fn)}
 
-class MAP2Dlassifier(APrioriClassifier):
+class MAP2DClassifier(APrioriClassifier):
 
     def __init__(self, df, attr):
         self.df = df
@@ -143,6 +145,7 @@ def nbParams(df, L):
 
     for e in L:
         res *= len({e for e in df[e]})
+
     return res * 8
 
 def nbParamsIndep(df):
@@ -150,5 +153,120 @@ def nbParamsIndep(df):
 
     for e in df:
         res += len({e for e in df[e]})
+
     return res * 8
 
+def drawNaiveBayes(df, attr):
+    res = ""
+
+    for a in {e for e in df}-{attr}:
+        res += attr+"->"+a+";"
+
+    res = res[:len(res)-1]
+    return utils.drawGraph(res)
+
+def nbParamsNaiveBayes(df, attr, attrs):
+    res = 0
+    tmp = len({e for e in df[attr]})
+
+    for e in attrs:
+        if e != attr:
+            res += len({e for e in df[e]}) * tmp
+
+    res += tmp
+    return res * 8
+
+class MLNaiveBayesClassifier(APrioriClassifier):
+
+    def __init__(self, df):
+        self.df = df
+        self.p = {e : P2D_l(df, e) for e in df if e != "target"}
+
+    def estimProbas(self, e):
+        res = {0:1, 1:1}
+        for key in e:
+            if key != "target":
+                if e[key] in self.p[key][0]:
+                    res[0] *= self.p[key][0][e[key]]
+                    res[1] *= self.p[key][1][e[key]]
+                else:
+                    res[0] *= 0
+                    res[1] *= 0
+        return res
+
+    def estimClass(self, e):
+        if self.estimProbas(e)[0] >= self.estimProbas(e)[1]:
+            return 0
+        return 1
+
+    def statsOnDF(self, data):
+        vp = 0
+        vn = 0
+        fp = 0
+        fn = 0
+
+        for i in range(len(data)):
+            e = data["target"][i]
+            if e == 0:
+                if self.estimClass(utils.getNthDict(data, i)) == 1:
+                    fp += 1
+                else:
+                    vn += 1
+            else:
+                if self.estimClass(utils.getNthDict(data, i)) == 1:
+                    vp += 1
+                else:
+                    fn += 1
+
+        return {"VP": vp, "VN": vn, "FP": fp, "FN": fn, "Précision": vp / (vp + fp), "Rappel": vp / (vp + fn)}
+
+class MAPNaiveBayesClassifier(APrioriClassifier):
+
+    def __init__(self, df):
+        self.df = df
+        self.p = {e: P2D_p(df, e) for e in df if e != "target"}
+
+    def estimProbas(self, e):
+        res = {0: 1, 1: 1}
+        for key in e:
+            if key != "target":
+                if e[key] in self.p[key]:
+                    res[0] *= self.p[key][e[key]][0]
+                    res[1] *= self.p[key][e[key]][1]
+                else:
+                    res[0] *= 0
+                    res[1] *= 0
+        return res
+
+    def estimClass(self, e):
+        if self.estimProbas(e)[0] >= self.estimProbas(e)[1]:
+            return 0
+        return 1
+
+    def statsOnDF(self, data):
+        vp = 0
+        vn = 0
+        fp = 0
+        fn = 0
+
+        for i in range(len(data)):
+            e = data["target"][i]
+            if e == 0:
+                if self.estimClass(utils.getNthDict(data, i)) == 1:
+                    fp += 1
+                else:
+                    vn += 1
+            else:
+                if self.estimClass(utils.getNthDict(data, i)) == 1:
+                    vp += 1
+                else:
+                    fn += 1
+
+        return {"VP": vp, "VN": vn, "FP": fp, "FN": fn, "Précision": vp / (vp + fp), "Rappel": vp / (vp + fn)}
+
+cl=MAPNaiveBayesClassifier(train)
+for i in [0,1,2]:
+    print("Estimation de la proba de l'individu {} par MAPNaiveBayesClassifier : {}".format(i,cl.estimProbas(utils.getNthDict(train,i))))
+    print("Estimation de la classe de l'individu {} par MAPNaiveBayesClassifier : {}".format(i,cl.estimClass(utils.getNthDict(train,i))))
+print("test en apprentissage : {}".format(cl.statsOnDF(train)))
+print("test en validation: {}".format(cl.statsOnDF(test)))
